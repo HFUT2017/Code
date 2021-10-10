a3e4e4f62a978593df7fac9b98c3fdedd39da4fb
fanyinghao <2824435408@qq.com>
2021-10-10 20:12:40 +0800

    ch8

---
+++ Basiclearning/Go/ch8/netcat1/netcat1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	defer conn.Close()
+	if err != nil {
+		log.Println(err)
+	}
+	mustCopy(os.Stdout, conn)
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatalln(err)
+	}
+}
